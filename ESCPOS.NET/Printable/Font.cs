d282bbf915d4b735739a82a1eaf24c82f1c1d43b
Ivan Montilla <montyclt@outlook.com>
2018-07-19 12:31:17 +0200

    Added font type printable class.

---
+++ ESCPOS.NET/Printable/Font.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace ESCPOS.NET.Printable
+{
+    public enum FontMode
+    {
+        FontA = 0,
+        FontB = 1,
+        Emphasized = 8,
+        DoubleHeight = 16,
+        DoubleWidth = 32,
+        Underline = 128,
+    }
+
+    public class Font : IPrintable
+    {
+        private FontMode[] _mode;
+
+        public Font(params FontMode[] mode)
+        {
+            if (mode == null)
+            {
+                mode = new[] {FontMode.FontA};
+            }
+
+            _mode = mode;
+        }
+
+        public Font(FontMode mode = FontMode.FontA)
+        {
+            _mode = new[] {mode};
+        }
+
+        public byte[] GetBytes()
+        {
+            var bytes = new List<byte>();
+
+            foreach (var mode in _mode)
+            {
+                bytes.AddRange(new[] { (byte)Control.Escape, (byte)'!' });
+                bytes.Add((byte) mode);
+            }
+
+            return bytes.ToArray();
+        }
+    }
+}
--- Usage/PrintImage.cs
+++ Usage/PrintImage.cs
@@ -12,9 +12,43 @@ namespace Usage
             IPrintConnector connector = new NetworkPrintConnector("10.0.0.50", 9100);
             IPrinter printer = new Printer(connector);
 
-            printer.Print("Empieza código de barras");
-            printer.Print(new Barcode("Hola"));
-            printer.Print("Termina código de barras");
+            printer.Print(new Justification(JustificationType.Center));
+            printer.Print(new Font(FontMode.Emphasized));
+            printer.Print("VADAVO");
+            printer.Feed();
+            printer.Feed();
+            printer.Print("Tienda VADAVO Quart");
+            printer.Print(new Font());
+            printer.Print("C. Quart 112 Bajo. 46008 Valencia");
+            printer.Print("960262326 - tienda.quart@vadavo.com");
+            printer.Feed();
+
+            printer.Reset();
+            
+            printer.Print(new Justification(JustificationType.Center));
+            printer.Print(new Font(FontMode.DoubleHeight, FontMode.Emphasized));
+            printer.Print("Ticket Nº 2018000001 - Fecha: 11/08/2018");
+            printer.Feed();
+            printer.Feed();
+            printer.Reset();
+
+            printer.Print("Cristal Templado iPhone X");
+            printer.Feed();
+            
+            printer.Print("iPhone X");
+            printer.Reset();
+            printer.Print(new Font(FontMode.FontB));
+            printer.Print("    S/N: AP-88662-232-21");
+            printer.Reset();
+            printer.Feed();
+            printer.Feed();
+
+
+            printer.Print(new Barcode("2018000001"));
+            printer.Print(new Justification(JustificationType.Center));
+            printer.Print("VADAVO SOLUCIONES S.L.");
+            printer.Print(new Font(FontMode.FontB));
+            printer.Print("CIF: B98455071");
             printer.Cut();
 
             Console.ReadKey();
