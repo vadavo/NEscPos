ae7b7dd387daf426a82d63702154ffd87f574242
Ivan Montilla <imontilla@vadavo.com>
2019-04-09 20:35:35 +0200

    Serial connector.

--- Vadavo.NEscPos/Connectors/SerialPortConnector.cs
+++ Vadavo.NEscPos/Connectors/SerialPortConnector.cs
@@ -6,15 +6,19 @@ namespace Vadavo.NEscPos.Connectors
     public class SerialPortConnector : IPrinterConnector
     {
         private readonly SerialPortStream _portStream;
+        private readonly BinaryWriter _writer;
+        private readonly BinaryReader _reader;
 
         public SerialPortConnector(string portName, int baudRate)
         {
             _portStream = new SerialPortStream(portName, baudRate);
+            _writer = new BinaryWriter(_portStream);
+            _reader = new BinaryReader(_portStream);
         }
         
         public void Write(byte[] data)
         {
-            
+            _writer.Write(data);
         }
 
         public byte[] Read()
@@ -24,6 +28,12 @@ namespace Vadavo.NEscPos.Connectors
         
         public void Dispose()
         {
+            _reader?.Close();
+            _reader?.Dispose();
+            
+            _writer?.Close();
+            _writer?.Dispose();
+            
             _portStream?.Close();
             _portStream?.Dispose();
         }
