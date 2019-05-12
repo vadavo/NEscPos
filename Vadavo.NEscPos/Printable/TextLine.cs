c2df39211f46af8cd1e7c11c9a1b0d763fd9cae1
Ivan Montilla <imontilla@vadavo.com>
2019-05-12 18:17:13 +0200

    TextLine printable type accepts codepage and encoding options.

--- Vadavo.NEscPos/Printable/TextLine.cs
+++ Vadavo.NEscPos/Printable/TextLine.cs
@@ -7,25 +7,36 @@ namespace Vadavo.NEscPos.Printable
 {
     public class TextLine : IPrintable
     {
-        private readonly string _line;
+        private readonly string _content;
         private readonly bool _feed;
+        private readonly byte _codePage;
+        private readonly Encoding _encoding;
 
-        public TextLine(string line = "", CodePage codePage = CodePage.Default, bool feed = true)
+        public TextLine(string content = "", CodePage codePage = CodePage.Default, Encoding encoding = null, bool feed = true)
         {
-            _line = line;
+            _content = content ?? throw new ArgumentNullException(nameof(content));
+            _codePage = (byte) codePage;
+            _encoding = encoding ?? DefaultEncoding;
             _feed = feed;
+        }
+
+        public TextLine(string content = "", byte codePage = 0, Encoding encoding = null, bool feed = true)
+        {
+            _content = content ?? throw new ArgumentNullException(nameof(content));
+            _feed = feed;
+            _codePage = codePage;
+            _encoding = encoding ?? DefaultEncoding;
         }
         
         public byte[] GetBytes()
         {
             var builder = new PrintableBuilder();
-            
-            var lineBytes = Encoding.Convert(Encoding.Unicode, Encoding.GetEncoding("ISO-8859-1"),
-                Encoding.Unicode.GetBytes(_line));
+
+            var contentBytes = _encoding.GetBytes(_content);
             
             builder
-                .Add(new SetCodePage(IsoCodePage))
-                .Add(lineBytes);
+                .Add(new SetCodePage(_codePage))
+                .Add(contentBytes);
 
             if (_feed)
                 builder.Add(new Feed());
@@ -34,29 +45,55 @@ namespace Vadavo.NEscPos.Printable
 
             return builder.ToArray();
         }
-
-        private byte IsoCodePage => 37;
+        
+        public static Encoding DefaultEncoding => Encoding.GetEncoding("ISO-8859-1");
     }
 
     public static class TextLineExtensions
     {
         /// <summary>
-        ///     Print a line of text using ISO-8859-3 codepage.
+        ///     Print a line of text.
         /// </summary>
-        public static void PrintLine(this IPrinter printer, string content = "")
+        public static void PrintLine(this IPrinter printer, string content = "", CodePage codePage = CodePage.Default,
+            Encoding encoding = null, bool feed = true)
         {
             if (printer == null)
                 throw new ArgumentNullException(nameof(printer));
             
-            printer.Print(new TextLine(content));
+            if (encoding == null)
+                encoding = TextLine.DefaultEncoding;
+            
+            printer.Print(new TextLine(content, codePage, encoding, feed));
         }
 
-        public static PrintableBuilder AddTextLine(this PrintableBuilder builder, string content = "")
+        public static void PrintLine(this IPrinter printer, string content, byte codePage, Encoding encoding,
+            bool feed = true)
+        {
+            if (printer == null)
+                throw new ArgumentNullException(nameof(printer));
+
+            if (encoding == null)
+                encoding = TextLine.DefaultEncoding;
+            
+            printer.Print(new TextLine(content, codePage, encoding, feed));
+        }
+
+        public static PrintableBuilder AddTextLine(this PrintableBuilder builder, string content = "",
+            CodePage codePage = CodePage.Default, Encoding encoding = null, bool feed = true)
         {
             if (builder == null)
                 throw new ArgumentNullException(nameof(builder));
 
-            return builder.Add(new TextLine(content));
+            return builder.Add(new TextLine(content, codePage, encoding, feed));
+        }
+
+        public static PrintableBuilder AddTextLine(this PrintableBuilder builder, string content, byte codePage,
+            Encoding encoding, bool feed = true)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return builder.Add(new TextLine(content, codePage, encoding, feed));
         }
     }
 }
